62e41812b5da8007429d9c94835a90147331fc8b
laurent <laurent.last7@gmail.com>
2023-05-26 17:56:29 +0200

    repositories

--- src/Model/Manager.php
+++ src/Model/Manager.php
@@ -7,24 +7,22 @@ use PDO;
 
 class Manager
 {
-    public function fetch(string $query)
+    public function fetch(string $query, array $param = [])
     {
-        $stmt = DBConnect::getPDO()->prepare("$query");
+        $stmt = DBConnect::getPDO()->prepare($query);
+        foreach ($param as $key => $value){
+            $stmt->bindParam(':' . $key, $value);
+        }
         $stmt->execute();
         return $stmt->fetch(PDO::FETCH_ASSOC);
     }
 
     public function fetchAll(string $query): bool|array
     {
-        $stmt = DBConnect::getPDO()->prepare("$query");
+        $stmt = DBConnect::getPDO()->prepare($query);
         $stmt->execute();
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
 
-    public function execute(string $query): bool
-    {
-        $stmt = DBConnect::getPDO()->prepare("$query");
-        return $stmt->execute();
-    }
 
 }

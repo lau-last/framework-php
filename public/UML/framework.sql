b676651f1f98638a61f0896e2d3bc5c73ea0e472
laurent <laurent.last7@gmail.com>
2023-06-28 18:04:17 +0200

    sql empty

--- public/UML/framework.sql
+++ public/UML/framework.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Generation Time: Jun 27, 2023 at 01:50 PM
+-- Generation Time: Jun 28, 2023 at 04:01 PM
 -- Server version: 5.7.24
 -- PHP Version: 8.1.0
 
@@ -37,15 +37,6 @@ CREATE TABLE `article` (
   `date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
--- Dumping data for table `article`
---
-
-INSERT INTO `article` (`id`, `user_id`, `title`, `head`, `content`, `date`) VALUES
-(1, 1, 'Futurama', 'Hey, what kinda party is this? There\'s no booze and only one hooker.', 'It must be wonderful. Hey, you add a one and two zeros to that or we walk! Fry, you can\'t just sit here in the dark listening to classical music. No, I\'m Santa Claus! I decline the title of Iron Cook and accept the lesser title of Zinc Saucier, which I just made up. Uhh… also, comes with double prize money.\r\n\r\nThen we\'ll go with that data file!\r\nAnd remember, don\'t do anything that affects anything, unless it turns out you were supposed to, in which case, for the love of God, don\'t not do it!\r\nHey, what kinda party is this? There\'s no booze and only one hooker.', '2023-06-27 14:54:10'),
-(2, 1, 'Doctor Who', 'I hate yogurt. It\'s just stuff with bits in.', 'Stop talking, brain thinking. Hush. I am the last of my species, and I know how that weighs on the heart so don\'t lie to me! All I\'ve got to do is pass as an ordinary human being. Simple. What could possibly go wrong?\r\n\r\nI\'m nobody\'s taxi service; I\'m not gonna be there to catch you every time you feel like jumping out of a spaceship.\r\nI\'m nobody\'s taxi service; I\'m not gonna be there to catch you every time you feel like jumping out of a spaceship.\r\nNo… It\'s a thing; it\'s like a plan, but with more greatness.', '2023-06-27 14:54:10'),
-(3, 1, 'Dexter', 'Like a sloth. I can do that.', 'I\'m real proud of you for coming, bro. I know you hate funerals. I have a dark side, too. Like a sloth. I can do that. I\'m real proud of you for coming, bro. I know you hate funerals.\r\n\r\nYou\'re a killer. I catch killers. You all right, Dexter? Makes me a … scientist. You all right, Dexter? I feel like a jigsaw puzzle missing a piece. And I\'m not even sure what the picture should be.', '2023-06-27 14:57:37');
-
 -- --------------------------------------------------------
 
 --
@@ -61,14 +52,6 @@ CREATE TABLE `comment` (
   `validation` enum('valid','invalid') NOT NULL DEFAULT 'invalid'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
--- Dumping data for table `comment`
---
-
-INSERT INTO `comment` (`id`, `user_id`, `article_id`, `content`, `date`, `validation`) VALUES
-(1, 2, 3, 'oh yes I remember. too funny', '2023-06-27 14:59:57', 'valid'),
-(2, 3, 1, 'i love futurama', '2023-06-27 15:00:54', 'valid');
-
 -- --------------------------------------------------------
 
 --
@@ -83,17 +66,9 @@ CREATE TABLE `user` (
   `role` enum('user','admin') NOT NULL DEFAULT 'user',
   `date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `token` varchar(255) NOT NULL,
+  `expiration_date` int(11) NOT NULL,
   `validation` enum('valid','invalid') NOT NULL DEFAULT 'invalid'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Dumping data for table `user`
---
-
-INSERT INTO `user` (`id`, `name`, `password`, `email`, `role`, `date`, `token`, `validation`) VALUES
-(1, 'laurent', '$2y$10$JaMIOulW.k5ZmHuSLXo4OOtcTjqFyEZor16NRKUCE3CqZfDl5jUiu', 'laurent@gmail.com', 'admin', '2023-06-27 14:36:13', '44785a6d31bf365bf9eb3e8d6f0c739d783fa4378516377baa19300adb9acc77', 'valid'),
-(2, 'aurélie', '$2y$10$SoleEIsX7B8eir3BG75OwuL1sUTuyUnm37SLVpqH3FP/1OasSN4Q2', 'aurelie@gmail.com', 'user', '2023-06-27 14:49:15', '6f2d085300a1ab5c5fbf326d15acec004ebfabeb97b456cbf31eb70406837be6', 'valid'),
-(3, 'sandrine', '$2y$10$ccUkAtkK6fc0tOeBVRR8WemNPRY6xqmrPbL9oLzeTrdxH9u8j6X66', 'sandrine@gmail.com', 'user', '2023-06-27 14:50:09', 'e1ae0e54f7cdd1d665cb7c7423032524f5605fa77ce47a4ec64fb8e076c9b0ca', 'valid');
 
 --
 -- Indexes for dumped tables
@@ -129,19 +104,19 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `article`
 --
 ALTER TABLE `article`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `comment`
 --
 ALTER TABLE `comment`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- Constraints for dumped tables
